6f64640e87b6a2d6f0514e4947db39938b517222
gustavopereiramendes1 <gustavo.pm2003@gmail.com>
2024-01-24 21:51:16 -0300

    Resolvendo conflitos

--- helloworld/website/urls.py
+++ helloworld/website/urls.py
@@ -1,8 +1,23 @@
-from . import views
 from django.urls import path
+from django.shortcuts import render
+from templates import views
+#from helloworld.models import Funcionario
 
 app_name = 'website'
 
 urlpatterns = [
     path('', views.index(), name='index'),
 ]
+
+def lista_funcionarios(request):
+    funcionarios = Funcionario.objetos.all()
+
+    contexto = {
+        'funcionarios':funcionarios
+    }
+
+    return render(
+        request,
+        "templates/funcionarios.html",
+        contexto
+    )
